37f18e3143571ca02dd588c3e99236462affe4d3
wangruijie <1293786141@qq.com>
2020-02-05 21:22:14 +0800

    添加点击流的web项目

---
+++ djl-web/src/main/java/com/xx/djlweb/DjlWebApplication.java
@@ -0,0 +1,13 @@
+package com.xx.djlweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlWebApplication.class, args);
+	}
+
+}
---
+++ djl-web/src/test/java/com/xx/djlweb/DjlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx.djlweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
